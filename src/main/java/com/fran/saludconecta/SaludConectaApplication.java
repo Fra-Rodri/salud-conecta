81aaa9ef1f5311c8145732b6e5c485b6f5b71903
Fra.Rod <fra.rod@outlook.com>
2025-10-09 21:07:57 +0200

    Comit inicial proyecto SaludConecta

---
+++ src/main/java/com/fran/saludconecta/SaludConectaApplication.java
@@ -0,0 +1,13 @@
+package com.fran.saludconecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludConectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludConectaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fran/saludconecta/SaludConectaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fran.saludconecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludConectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
