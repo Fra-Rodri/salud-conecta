32c61b74fae3178bf86221a286166013c487533c
Fra.Rod <fra.rod@outlook.com>
2025-10-19 21:44:10 +0200

    Corrijo script de creación

--- src/main/resources/Sql/Create.sql
+++ src/main/resources/Sql/Create.sql
@@ -1,6 +1,8 @@
 -- Crear esquema si no existe
 --CREATE SCHEMA IF NOT EXISTS clinica;
 
+
+-- Trigger para actualizar el campo fecha modificación
 CREATE OR REPLACE FUNCTION actualizar_fecha_modificacion()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -8,6 +10,11 @@ BEGIN
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+
+-- Enumeradores para roles y estados
+CREATE TYPE rol_usuario AS ENUM ('admin', 'profesional', 'recepcion');
+
 
 -- Tabla negocio
 CREATE TABLE clinica.negocio (
@@ -25,19 +32,21 @@ BEFORE UPDATE ON clinica.negocio
 FOR EACH ROW
 EXECUTE FUNCTION actualizar_fecha_modificacion();
 
+
+
 -- Tabla usuario (profesionales)
 CREATE TABLE clinica.usuario (
 	id serial4 NOT NULL,
 	nombre varchar(100) NOT NULL,
 	email varchar(100) NOT NULL,
 	password varchar(255) NOT NULL,
-	rol varchar(50) NOT NULL,
-	negocio_id int4 NOT NULL,
+	rol rol_usuario NOT NULL,
+	negocio_id int4, --  NOT NULL, NO ES NECESARIO QUE PERTENEZCA A UN NEGOCIO, PUEDE SER UN TRABAJADOR DE LA EMPRESA SIN MÁS
 	fecha_creacion timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	fecha_modificacion timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT usuario_pk PRIMARY KEY (id),
-	CONSTRAINT usuario_email_uk UNIQUE (email),
-	CONSTRAINT usuario_negocio_id_fk FOREIGN KEY (negocio_id) REFERENCES clinica.negocio(id) -- Si se borra un negocio no se borra el usuario
+	CONSTRAINT usuario_email_uk UNIQUE (email) --,
+	--CONSTRAINT usuario_negocio_id_fk FOREIGN KEY (negocio_id) REFERENCES clinica.negocio(id) ON DELETE SET NULL -- Si se borra un negocio no se borra el usuario, pero se le pone la fk a null, de esta manera al crear el usuario le valida en que negocio trabaja
 );
 
 CREATE INDEX usuario_email_idx ON clinica.usuario (email);
@@ -47,25 +56,30 @@ BEFORE UPDATE ON clinica.usuario
 FOR EACH ROW
 EXECUTE FUNCTION actualizar_fecha_modificacion();
 
+
+
 -- Tabla paciente
 CREATE TABLE clinica.paciente (
     id serial4 NOT NULL,
     nombre VARCHAR(100) NOT NULL,
     dni VARCHAR(20) UNIQUE NOT NULL,
     fecha_nacimiento DATE,
-    negocio_id int4 NOT NULL,
+    alta boolean DEFAULT TRUE,
+    -- negocio_id int4 NOT NULL, -- NO TIENE SENTIDO TENER UN PACIENTE ASOCIADO A UN NEGOCIO
     fecha_creacion timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     fecha_modificacion timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT paciente_pk PRIMARY KEY (id),
-    CONSTRAINT paciente_negocio_id_fk FOREIGN KEY (negocio_id) REFERENCES clinica.negocio(id) -- Si se elimina un negocio NO se elimina el paciente
+    CONSTRAINT paciente_pk PRIMARY KEY (id) --,
+    --CONSTRAINT paciente_negocio_id_fk FOREIGN KEY (negocio_id) REFERENCES clinica.negocio(id) -- Si se elimina un negocio NO se elimina el paciente
 );
 
-CREATE INDEX paciente_nombre_idx ON clinica.paciente (nombre);
+CREATE INDEX paciente_nombre_idx ON clinica.paciente(nombre);
 
 CREATE TRIGGER paciente_fecha_modificacion_trigger
 BEFORE UPDATE ON clinica.paciente
 FOR EACH ROW
 EXECUTE FUNCTION actualizar_fecha_modificacion();
+
+
 
 -- Relación N:M entre pacientes y usuarios
 CREATE TABLE clinica.paciente_usuario (
@@ -82,6 +96,8 @@ CREATE TRIGGER paciente_usuario_fecha_modificacion_trigger
 BEFORE UPDATE ON clinica.paciente_usuario
 FOR EACH ROW
 EXECUTE FUNCTION actualizar_fecha_modificacion();
+
+
 
 -- Tabla cita
 CREATE TABLE clinica.cita (
@@ -104,19 +120,22 @@ FOR EACH ROW
 EXECUTE FUNCTION actualizar_fecha_modificacion();
 
 
+
 -- Tabla informe clínico
 CREATE TABLE clinica.informe (
     id serial4 NOT NULL,
-    cita_id INTEGER NOT NULL,
-    usuario_id INTEGER NOT NULL,
-    paciente_id INTEGER NOT NULL,
+    --cita_id INTEGER NOT NULL, --NO TIENE SENTIDO QUE UN INFORME SEA DE UNA CITA, MAS BIEN DEBERIA DE SER DE UN PACIENTE SIN MAS
+    usuario_id INTEGER,
+    nombre_usuario VARCHAR(100),
+    paciente_id INTEGER,
+    nombre_paciente VARCHAR(100),
     contenido TEXT NOT NULL,
     fecha_creacion timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     fecha_modificacion timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT informe_pk PRIMARY KEY (id),
-    CONSTRAINT informe_cita_id_fk FOREIGN KEY (cita_id) REFERENCES clinica.cita(id), -- Si se elimina una cita, no se eliminan automáticamente el informe
-    CONSTRAINT informe_usuario_id_fk FOREIGN KEY (usuario_id) REFERENCES clinica.usuario(id), -- Si se elimina un usuario (profesional), no se eliminan los informes
-    CONSTRAINT informe_paciente_id_fk FOREIGN KEY (paciente_id) REFERENCES clinica.paciente(id) -- Si se elimina un paciente, no se eliminan sus informes
+   --CONSTRAINT informe_cita_id_fk FOREIGN KEY (cita_id) REFERENCES clinica.cita(id), -- Si se elimina una cita, no se eliminan automáticamente el informe
+    CONSTRAINT informe_usuario_id_fk FOREIGN KEY (usuario_id) REFERENCES clinica.usuario(id) ON DELETE SET NULL, -- Si se elimina un usuario (profesional), no se eliminan los informes, pero su fk se pone a null, esto ayuda que el informe solo se pueda crear si el fk existe
+    CONSTRAINT informe_paciente_id_fk FOREIGN KEY (paciente_id) REFERENCES clinica.paciente(id) ON DELETE SET NULL -- Si se elimina un paciente, no se eliminan sus informes, pero su fk se pone a null, esto ayuda que el informe solo se pueda crear si el fk existe
 ); -- De esta forma el historial de informes siempre será guardado al no ser que se borre explicitamente
 
 CREATE TRIGGER informe_fecha_modificacion_trigger
